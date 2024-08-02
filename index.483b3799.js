bb839d934e59357d978e01b4e00c5e12180267ca
Roma Zuravliov <janmorozov1988@gmail.com>
2024-08-02 17:10:17 +0300

    make build

---
+++ index.483b3799.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e,t){document.querySelector(".logo").addEventListener("click",function(){e()})}),t=new Promise(function(e,t){setTimeout(function(){t(Error("Promise rejected after 3 seconds"))},3e3)});function n(){var e=document.createElement("div");e.className="message",e.textContent="Promise was resolved!",document.body.appendChild(e)}function o(){var e=document.createElement("div");e.className="error-message",e.textContent="Promise was rejected!",document.body.appendChild(e)}e.then(n).catch(o),t.then(n).catch(o);
+//# sourceMappingURL=index.483b3799.js.map
